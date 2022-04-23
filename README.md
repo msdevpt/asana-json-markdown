49a045b871c409846f0c2b6018e6e3669094c0ed
M.Silva <msilva.kb@outlook.com>
2022-04-23 14:46:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# asana-json-markdown
+A simple JSON to markdown convert for [Asana](https://github.com/Asana) export tasks file.
+
+## Getting Started
+
+```bash
+$ git clone https://github.com/msdevpt/asana-json-markdown
+$ cd asana-json-markdown\src
+$ dotnet run in.json out.md
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+
+## :rocket: Technologies
+
+This project was developed with the following technologies:
+
+- [.NET 6](https://dotnet.microsoft.com/)
---
+++ src/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Text.Json;
+using models;
+
+if (Environment.GetCommandLineArgs().Count() == 2)
+{
+    Console.WriteLine("Usage: app.exe <in.json> <out.md>");
+    Environment.Exit(1);
+}
+
+var fileName = Environment.GetCommandLineArgs()[1];
+if (!File.Exists(fileName))
+{
+    Console.WriteLine($"File {fileName} not found");
+    Environment.Exit(1);
+}
+
+string jsonString = System.IO.File.ReadAllText(fileName);
+Root? tasks = JsonSerializer.Deserialize<Root>(jsonString);
+
+if (tasks is null)
+{
+    Console.WriteLine("Invalid JSON");
+    Environment.Exit(1);
+}
+
+StringBuilder sb = GetTasksConverted(tasks);
+
+var fileDest = Environment.GetCommandLineArgs()[2];
+SaveTasks(sb, fileDest);
+
+Console.WriteLine("File successfully converted");
+
+
+static StringBuilder GetTasksConverted(Root? tasks)
+{
+  var sb = new StringBuilder();
+  foreach (var task in tasks.data)
+  {
+    sb.AppendLine("- **" + task.name + "** - due " + task.due_on);
+    foreach (var subtask in task.subtasks)
+    {
+      sb.AppendLine("\t- " + subtask.name);
+    }
+  }
+
+  return sb;
+}
+
+static void SaveTasks(StringBuilder sb, string fileDest)
+{
+  StreamWriter sw = new StreamWriter(fileDest, true);
+  sw.Write(sb.ToString());
+  sw.Close();
+}
