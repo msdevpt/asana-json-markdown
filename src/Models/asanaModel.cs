b765d4acaaadf224bcdeec57493ea9e661e8321c
M.Silva <msilva.kb@outlook.com>
2022-04-28 19:00:01 +0100

    fix: load args in debug/release

--- src/Models/asanaModel.cs
+++ src/Models/asanaModel.cs
@@ -1,6 +1,6 @@
 // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
 
-namespace models {
+namespace Models{
     public class Assignee
     {
         public string gid { get; set; }
@@ -15,55 +15,11 @@ namespace models {
         public string resource_type { get; set; }
     }
 
-    public class EnumOption
-    {
-        public string gid { get; set; }
-        public string color { get; set; }
-        public bool enabled { get; set; }
-        public string name { get; set; }
-        public string resource_type { get; set; }
-    }
-
-    public class CustomField
-    {
-        public string gid { get; set; }
-        public bool enabled { get; set; }
-        public List<EnumOption> enum_options { get; set; }
-        public object enum_value { get; set; }
-        public string name { get; set; }
-        public object created_by { get; set; }
-        public object display_value { get; set; }
-        public string resource_subtype { get; set; }
-        public string resource_type { get; set; }
-        public string type { get; set; }
-        public object number_value { get; set; }
-        public int? precision { get; set; }
-    }
-
     public class Follower
     {
         public string gid { get; set; }
         public string name { get; set; }
         public string resource_type { get; set; }
-    }
-
-    public class User
-    {
-        public string gid { get; set; }
-        public string name { get; set; }
-        public string resource_type { get; set; }
-    }
-
-    public class Heart
-    {
-        public string gid { get; set; }
-        public User user { get; set; }
-    }
-
-    public class Like
-    {
-        public string gid { get; set; }
-        public User user { get; set; }
     }
 
     public class Project
@@ -113,9 +69,9 @@ namespace models {
         public object assignee { get; set; }
         public string assignee_status { get; set; }
         public bool completed { get; set; }
-        public DateTime completed_at { get; set; }
+        public DateTime? completed_at { get; set; }
         public DateTime created_at { get; set; }
-        public List<CustomField> custom_fields { get; set; }
+        public List<object> custom_fields { get; set; }
         public object due_at { get; set; }
         public object due_on { get; set; }
         public List<Follower> followers { get; set; }
@@ -148,16 +104,16 @@ namespace models {
         public string assignee_status { get; set; }
         public AssigneeSection assignee_section { get; set; }
         public bool completed { get; set; }
-        public DateTime completed_at { get; set; }
+        public DateTime? completed_at { get; set; }
         public DateTime created_at { get; set; }
-        public List<CustomField> custom_fields { get; set; }
+        public List<object> custom_fields { get; set; }
         public object due_at { get; set; }
         public string due_on { get; set; }
         public List<Follower> followers { get; set; }
         public bool hearted { get; set; }
-        public List<Heart> hearts { get; set; }
+        public List<object> hearts { get; set; }
         public bool liked { get; set; }
-        public List<Like> likes { get; set; }
+        public List<object> likes { get; set; }
         public List<Membership> memberships { get; set; }
         public DateTime modified_at { get; set; }
         public string name { get; set; }
